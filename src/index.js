454e7c0bd042b0336d130dce31393f4fa04fcb86
dbukatyy <dbukatyy@mail.ru>
2017-09-27 14:40:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 module.exports = function zeros(expression) {
   // your solution
-  const number = parseInt(expression);
+  const expArr = expression.split('*');
+  
+  let resArr = expArr.map((i) => {
+    return {
+      number: parseInt(i),
+      iter: itemCounter(i, '!') 
+    }
+  });
+  
+  let resNumber = resArr.reduce((mult,i) => {
+    return mult *= nFactorial(i.iter, i.number);
+  },1);
+  
+  function itemCounter(str, item) {
+      let pos = -1,
+      count = 0;
+    
+      while ((pos = str.indexOf(item, pos + 1)) != -1) {
+        count += 1
+      }
+  
+    return count
+  }
   
   function factorial(n) {
     return (n != 1) ? n * factorial(n - 1) : 1;
   }
   
-  const factorialNumber = factorial(number),
-        str = String(factorialNumber);
-        
-  let pos = -1,
-      count = 0;
+  function nFactorial(n, number) {
+    let res = number;
     
-  while ((pos = str.indexOf('0', pos + 1)) != -1) {
-    count += 1
+    while( n > 0) {
+      res = factorial(res)
+      n--
+    }
+    
+    return res;
   }
   
-  return count
+  return itemCounter(String(resNumber), 0)
 }
