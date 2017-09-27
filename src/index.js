fcba3fae5fcaf9d9fe6cb9e3cc6813f73b42674f
dbukatyy <dbukatyy@mail.ru>
2017-09-27 09:57:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function zeros(expression) {
   // your solution
+  const number = parseInt(expression);
+  
+  function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+  }
+  
+  const factorialNumber = factorial(number),
+        str = String(factorialNumber);
+        
+  let pos = -1,
+      count = 0;
+    
+  while ((pos = str.indexOf(0, pos + 1)) != -1) {
+    count += 1
+  }
+  
+  return count
 }
